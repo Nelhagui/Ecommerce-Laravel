c65dd7c49d60e0108c2173fbf4f08a279b285ba6
Nelhagui <nelsonaguiarsalazar@gmail.com>
2018-11-25 07:31:53 -0300

    slider, scrips

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,9 +106,16 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="main">
             @yield('content')
         </main>
     </div>
+
+    <script src="js/app.js"></script>
+    <script src="js/master.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/holder.min.js"></script>
 </body>
 </html>
