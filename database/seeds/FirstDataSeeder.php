f664a3aa84fb0082e912baaa248eb0dad14caa16
Lucas Emiliani <lucasemiliani1@hotmail.com>
2018-12-10 20:04:30 -0300

    botones de home llevan a servicios de esa categoria

--- database/seeds/FirstDataSeeder.php
+++ database/seeds/FirstDataSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
+use App\Service;
+use App\User;
+
+
 
 class FirstDataSeeder extends Seeder
 {
@@ -14,5 +19,28 @@ class FirstDataSeeder extends Seeder
         factory(App\User::class, 15)->create();
         factory(App\Category::class, 11)->create();
         factory(App\Service::class, 15)->create();
+
+       $deportes = new Category([
+        'id' => '12',
+        'name' =>  'Deportes',
+        'description' => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, eveniet?",
+       ]);
+       $deportes->save();
+       
+
+       $gastronomia = new Category([
+           'id' => '13',
+           'name' =>  'Gastronomia',
+           'description' => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, eveniet?",
+       ]);
+       $gastronomia->save();
+      
+
+       $educacion = new Category([
+        'id' => '14',
+        'name' =>  'Educacion',
+        'description' => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, eveniet?",
+       ]);
+       $educacion->save();
     }
 }
